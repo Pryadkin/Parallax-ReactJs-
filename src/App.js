971e1cce5cfe1b0de8c61eab864380a4d79a4226
pryadkin.a <pryadkin.a@citilink.ru>
2019-08-30 13:35:24 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.top = 50;
+    this.left = 50;
 
+    this.state = {
+      sqX: this.top,
+      sqY: this.left,
+    }
+
+    this.styleApp = {
+      width: "100%",
+      height: "500px",
+      border : "1px solid black",
+      position: "relative",
+    }
+  }
+
+  onMouseMove = (e) => {
+    this.setState({
+      sqX: -e.clientX/1000 + this.top,
+      sqY: -e.clientY/100 + this.left,
+    })
+  }
+
+  render() {
+    this.styleSquare = {
+      width: "110%",
+      height: "110vh",
+      backgroundColor: "black",
+      position: "absolute",
+      top: `${this.state.sqY}%`,
+      left: `${this.state.sqX}%`,
+      transform: "translate(-50%, -50%)",
+      backgroundImage: "url('/img/image.jpg')",
+      backgroundRepeat: `no-repeat`,
+      backgroundPosition: "center",
+      backgroundSize: "cover"
+    }
+    return (
+      <div className="App" style={this.styleApp} onMouseMove={this.onMouseMove}>
+        <div className="square" style={this.styleSquare}></div>
+      </div>
+    );
+  }
+}
 export default App;
